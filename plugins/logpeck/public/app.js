7c10bc017f8846abef5fd224ffca73bf2a2803ca
xuanyuz <zxy_helloworld@163.com>
2017-10-27 19:12:45 +0800

    perfect the functions

--- plugins/logpeck/public/app.js
+++ plugins/logpeck/public/app.js
@@ -8,27 +8,84 @@ import template from './templates/index.html';
 
 uiRoutes.enable();
 uiRoutes
-.when('/', {
-  template,
-  resolve: {
-    currentTime($http) {
-      return $http.get('../api/logpeck/example').then(function (resp) {
-        return resp.data.time;
-      });
-    }
-  }
-});
+  .when('/', {
+    template : template,
+    controller : 'logpeckHelloWorld',
+  });
 
 uiModules
 .get('app/logpeck', [])
-.controller('logpeckHelloWorld', function ($scope, $route, $interval) {
-  $scope.title = 'Logpeck';
-  $scope.description = '管理监控任务';
+.controller('logpeckHelloWorld',function ($scope , $http) {
+  //初始化
+  $http({
+    method: 'GET',
+    url: '../api/logpeck/init'
+  }).then(function successCallback() {
 
-  const currentTime = moment($route.current.locals.currentTime);
-  $scope.currentTime = currentTime.format('HH:mm:ss');
-  const unsubscribe = $interval(function () {
-    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
-  }, 1000);
-  $scope.$watch('$destroy', unsubscribe);
+  }, function errorCallback() {
+
+  });
+
+
+  //list task
+  $scope.listTask = function () {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/list'
+    }).then(function successCallback(response) {
+      console.log(response['data'][0]['Name']);
+      var new_arr = [];
+      for (var id=0 ; id<response['data'].length ; id++) {
+        new_arr.push(response['data'][id]['Name']);
+      }
+      $scope.T_Name=new_arr;
+    }, function errorCallback() {
+    });
+  };
+
+
+  //startTask
+  $scope.startTask = function ($event) {
+    //console.log(event.target.getAttribute('name'));
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/start',
+      data: {name: event.target.getAttribute('name')},
+    }).then(function successCallback(response) {
+      //console.log(response['data'][0]['Name']);
+    }, function errorCallback() {
+    });
+  };
+
+
+  //stopTask
+  $scope.stopTask = function ($event) {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/stop',
+      data: {name: event.target.getAttribute('name')},
+    }).then(function successCallback(response) {
+      //console.log(response['data'][0]['Name']);
+    }, function errorCallback() {
+    });
+  };
+
+
+  //removeTask
+  $scope.removeTask = function ($event) {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/remove',
+      data: {name: event.target.getAttribute('name')},
+    }).then(function successCallback(response) {
+      //console.log(response['data'][0]['Name']);
+    }, function errorCallback() {
+    });
+  };
+
+
 });
+
+
+
+
--- plugins/logpeck/server/routes/example.js
+++ plugins/logpeck/server/routes/example.js
@@ -1,11 +1,117 @@
-export default function (server) {
+import Wreck from 'wreck';
 
-  server.route({
-    path: '/api/logpeck/example',
-    method: 'GET',
-    handler(req, reply) {
-      reply({ time: (new Date()).toISOString() });
-    }
-  });
+export default function (server) {
+  server.route([
+    {
+      path: '/api/logpeck/init',
+      method: 'GET',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          Wreck.post('http://localhost:7117/peck_task/list',
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/list',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          Wreck.post('http://localhost:7117/peck_task/list',
+          (err, xyResponse, payload) => {
+            if (err) {
+              console.log(err);
+            }
+            reply(payload.toString());
+          });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/start',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          //console.log(req.payload.name);
+          var name=req.payload.name;
+          Wreck.post('http://localhost:7117/peck_task/start',{ payload: '{ "Name" : "'+name+'" }' },
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/stop',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          var name=req.payload.name;
+          Wreck.post('http://localhost:7117/peck_task/stop',{ payload: '{ "Name" : "'+name+'" }' },
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/remove',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          var name=req.payload.name;
+          Wreck.post('http://localhost:7117/peck_task/remove',{ payload: '{ "Name" : "'+name+'" }' },
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+  ]);
 
 }
