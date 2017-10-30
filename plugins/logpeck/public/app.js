721da9f95e16f5c9eed3eaf446ac8f877f3837eb
xuanyuz <zxy_helloworld@163.com>
2017-10-30 17:57:32 +0800

    perfect operator interface

--- plugins/logpeck/public/app.js
+++ plugins/logpeck/public/app.js
@@ -4,42 +4,66 @@ import uiRoutes from 'ui/routes';
 
 import 'ui/autoload/styles';
 import './less/main.less';
-import template from './templates/index.html';
+import template1 from './templates/index.html';
+import template2 from './templates/addTask.html';
+import template3 from './templates/addHost.html';
 
 uiRoutes.enable();
 uiRoutes
   .when('/', {
-    template : template,
-    controller : 'logpeckHelloWorld',
+    template : template1,
+    controller : 'logpeckInit',
+  })
+  .when('/addTask', {
+    template : template2,
+    controller : 'logpeckInit',
+  })
+  .when('/addHost', {
+    template : template3,
+    controller : 'logpeckInit',
   });
-
+var G_ip='';
 uiModules
 .get('app/logpeck', [])
-.controller('logpeckHelloWorld',function ($scope , $http) {
+.controller('logpeckInit',function ($scope , $rootScope, $http,) {
   //初始化
   $http({
-    method: 'GET',
-    url: '../api/logpeck/init'
-  }).then(function successCallback() {
-
+    method: 'POST',
+    url: '../api/logpeck/init',
+    data: {ip:G_ip},
+  }).then(function successCallback(response) {
+    //console.log(response['data']['hits']['hits'][0]['_source']['ip']);
+    //console.log(response['data']['hits']['total']);
+    var new_arr = [];
+    for (var id=0 ; id<response['data']['hits']['total'] ; id++) {
+      new_arr.push(response['data']['hits']['hits'][id]['_source']['ip']);
+    }
+    $scope.T_IpList=new_arr;
+    $scope.T_ip=G_ip;
+    $scope.visible=false;
+    //console.log($rootScope.G_ip);
   }, function errorCallback() {
 
   });
 
 
   //list task
-  $scope.listTask = function () {
+  $scope.listTask = function ($event) {
     $http({
       method: 'POST',
-      url: '../api/logpeck/list'
+      url: '../api/logpeck/list',
+      data: {ip: event.target.getAttribute('name')},
     }).then(function successCallback(response) {
-      console.log(response['data'][0]['Name']);
+      $scope.T_ip=response.config.data.ip;    //?????????????????????????????????????????????????????????????????
+      G_ip=response.config.data.ip;
+      $scope.visible=true;
       var new_arr = [];
       for (var id=0 ; id<response['data'].length ; id++) {
         new_arr.push(response['data'][id]['Name']);
       }
       $scope.T_Name=new_arr;
     }, function errorCallback() {
+
     });
   };
 
@@ -50,7 +74,7 @@ uiModules
     $http({
       method: 'POST',
       url: '../api/logpeck/start',
-      data: {name: event.target.getAttribute('name')},
+      data: {name: event.target.getAttribute('name'),ip: $scope.T_ip},
     }).then(function successCallback(response) {
       //console.log(response['data'][0]['Name']);
     }, function errorCallback() {
@@ -63,7 +87,7 @@ uiModules
     $http({
       method: 'POST',
       url: '../api/logpeck/stop',
-      data: {name: event.target.getAttribute('name')},
+      data: {name: event.target.getAttribute('name'),ip: $scope.T_ip},
     }).then(function successCallback(response) {
       //console.log(response['data'][0]['Name']);
     }, function errorCallback() {
@@ -76,15 +100,57 @@ uiModules
     $http({
       method: 'POST',
       url: '../api/logpeck/remove',
-      data: {name: event.target.getAttribute('name')},
+      data: {name: event.target.getAttribute('name'),ip: $scope.T_ip},
     }).then(function successCallback(response) {
+      //angular.element("#listTask").click();
+      $("#listTask").click($event);
       //console.log(response['data'][0]['Name']);
     }, function errorCallback() {
     });
   };
 
+  $scope.addTask = function () {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/addTask',
+      data: {name: $scope.Name,logpath: $scope.LogPath,hosts: $scope.Hosts,index:$scope.Index,type:$scope.Type},
+    }).then(function successCallback(response) {
+      //$state.go('/');
+      //$state.go('producer', {producerId: producerId});
+      //angular.element("#listTask").click();
+    }, function errorCallback() {
+    });
+  };
+
+  $scope.addHost = function () {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/addHost',
+      data: {ip: $scope.IP},
+    }).then(function successCallback(response) {
+      //$state.go('/');
+      //$state.go('producer', {producerId: producerId});
+      //angular.element("#listTask").click();
+    }, function errorCallback() {
+    });
+  };
+
+  $scope.removeHost = function ($event) {
+    $http({
+      method: 'POST',
+      url: '../api/logpeck/removeHost',
+      data:{ip: event.target.getAttribute('name')},
+    }).then(function successCallback(response) {
+      //$state.go('/');
+      //$state.go('producer', {producerId: producerId});
+      //angular.element("#listTask").click();
+    }, function errorCallback() {
+    });
+  };
 
 });
+
+
 
 
 
--- plugins/logpeck/server/routes/example.js
+++ plugins/logpeck/server/routes/example.js
@@ -4,23 +4,25 @@ export default function (server) {
   server.route([
     {
       path: '/api/logpeck/init',
-      method: 'GET',
+      method: 'POST',
       handler(req, reply) {
         const Wreck = require('wreck');
         const example = async function () {
-          Wreck.post('http://localhost:7117/peck_task/list',
+          var ip=req.payload.ip;
+          console.log(ip);
+          Wreck.post('http://localhost:9200/logpeck/host/_search?q=*&pretty',
             (err, xyResponse, payload) => {
               if (err) {
                 console.log(err);
               }
               reply(payload.toString());
+
             });
         };
         try {
           example();
         }
         catch (err) {
-
         }
       }
     },
@@ -30,7 +32,8 @@ export default function (server) {
       handler(req, reply) {
         const Wreck = require('wreck');
         const example = async function () {
-          Wreck.post('http://localhost:7117/peck_task/list',
+          var ip=req.payload.ip;
+          Wreck.post('http://'+ip+':7117/peck_task/list',
           (err, xyResponse, payload) => {
             if (err) {
               console.log(err);
@@ -51,9 +54,8 @@ export default function (server) {
       handler(req, reply) {
         const Wreck = require('wreck');
         const example = async function () {
-          //console.log(req.payload.name);
           var name=req.payload.name;
-          Wreck.post('http://localhost:7117/peck_task/start',{ payload: '{ "Name" : "'+name+'" }' },
+          Wreck.post('http://'+ip+':7117/peck_task/start',{ payload: '{ "Name" : "'+name+'" }' },
             (err, xyResponse, payload) => {
               if (err) {
                 console.log(err);
@@ -75,7 +77,7 @@ export default function (server) {
         const Wreck = require('wreck');
         const example = async function () {
           var name=req.payload.name;
-          Wreck.post('http://localhost:7117/peck_task/stop',{ payload: '{ "Name" : "'+name+'" }' },
+          Wreck.post('http://'+ip+':7117/peck_task/stop',{ payload: '{ "Name" : "'+name+'" }' },
             (err, xyResponse, payload) => {
               if (err) {
                 console.log(err);
@@ -97,12 +99,90 @@ export default function (server) {
         const Wreck = require('wreck');
         const example = async function () {
           var name=req.payload.name;
-          Wreck.post('http://localhost:7117/peck_task/remove',{ payload: '{ "Name" : "'+name+'" }' },
+          var ip=req.payload.ip;
+          Wreck.post('http://'+ip+':7117/peck_task/remove',{ payload: '{ "Name" : "'+name+'" }' },
             (err, xyResponse, payload) => {
               if (err) {
                 console.log(err);
               }
               reply(payload.toString());
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/addTask',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          var name=req.payload.name;
+          var logpath=req.payload.logpath;
+          var hosts=req.payload.hosts;
+          var index=req.payload.index;
+          var type=req.payload.type;
+          console.log(req.payload);
+          Wreck.post('http://localhost:7117/peck_task/add',{ payload: '{ "Name" : "'+name+'","LogPath":"'+logpath+'","ESConfig":{"Hosts":["'+hosts+'"],"Index":"'+index+'","Type":"'+type+'"} }' },
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/addHost',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          var ip=req.payload.ip;
+          Wreck.post('http://localhost:9200/logpeck/host',{ payload: '{ "ip" : "'+ip+'" }' },
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+
+            });
+        };
+        try {
+          example();
+        }
+        catch (err) {
+        }
+      }
+    },
+    {
+      path: '/api/logpeck/removeHost',
+      method: 'POST',
+      handler(req, reply) {
+        const Wreck = require('wreck');
+        const example = async function () {
+          var ip=req.payload.ip;
+          //Wreck.delete('http://localhost:9200/logpeck/host/_query?',{payload:'{"query": { "match": {"ip":"'+ip+'"}}}'},
+          Wreck.post('http://localhost:9200/logpeck/host/_search?_source=false',{payload:'{"query": { "match": {"ip":"'+ip+'"}}}'},
+            (err, xyResponse, payload) => {
+              if (err) {
+                console.log(err);
+              }
+              reply(payload.toString());
+              var getid=JSON.parse(payload.toString());
+              var id=getid['hits']['hits'][0]['_id'];
+              Wreck.delete('http://localhost:9200/logpeck/host/'+id+'?',(err, xyResponse, payload) => {});
             });
         };
         try {
