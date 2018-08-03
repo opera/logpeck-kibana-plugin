973e80c20a47861a71205286ed4577935fbc3703
xuanyuz <zxy_helloworld@163.com>
2018-08-03 18:48:55 +0800

    Create elasticsearch index

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo cp -R ./logpeck-kibana-plugin/plugins/logpeck ./plugins/logpeck
+sudo cp -r ./logpeck-kibana-plugin/plugins/logpeck ./plugins/logpeck
+sudo curl -XPUT '127.0.0.1:9200/logpeck/'
+sudo curl -XPUT '127.0.0.1:9200/logpeck_group/'
+sudo curl -XPUT '127.0.0.1:9200/.logpeck/'
