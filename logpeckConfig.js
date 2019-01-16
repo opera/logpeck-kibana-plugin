3156f021991029e8951fac99567abf48c09c32cb
xuanyuz <xuanyuz123@gmail.com>
2019-01-16 16:03:08 +0800

    Modify db path

--- logpeckConfig.js
+++ logpeckConfig.js
@@ -54,9 +54,9 @@ export const DefaultLogpeckIP = "127.0.0.1:7117";
 
 //***************************************************
 export const Config = {
-  HostName:__dirname+'/../db/host-name.txt',
-  GroupName:__dirname+'/../db/group-name.txt',
-  TemplateName:__dirname+'/../db/template-name.txt',
-  GroupMemberPath:__dirname+'/../db/group/', //group-member-{{groupName}}.txt
-  TemplatePath:__dirname+'/../db/template/',
+  HostName:__dirname+'/db/host-name.txt',
+  GroupName:__dirname+'/db/group-name.txt',
+  TemplateName:__dirname+'/db/template-name.txt',
+  GroupMemberPath:__dirname+'/db/group/', //group-member-{{groupName}}.txt
+  TemplatePath:__dirname+'/db/template/',
 };
